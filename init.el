c447330a3e816367842b36b4967a2c1369b7114e
trevstanhope <tpstanhope@gmail.com>
2012-09-13 13:32:26 -0400

    regular reload

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
  '(inhibit-default-init nil)
  '(inhibit-startup-screen t)
  '(initial-buffer-choice t)
@@ -53,5 +54,4 @@
 
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-	(cons '("\\.md" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
