b5c00c9ce8c301c806043b29ca05faa33027df3d
trevstanhope <tpstanhope@gmail.com>
2012-11-15 12:38:03 -0500

    all

--- init.el
+++ init.el
@@ -54,4 +54,4 @@
 
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
-(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist)) 
